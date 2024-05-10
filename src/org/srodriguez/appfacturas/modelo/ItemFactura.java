6730d783f7706aa47e46229bbbcd8745c4a4179f
Shagooh <stgorodu@gmail.com>
2024-05-09 22:34:29 -0400

    first commit

---
+++ src/org/srodriguez/appfacturas/modelo/ItemFactura.java
@@ -0,0 +1,14 @@
+package org.srodriguez.appfacturas.modelo;
+
+public class ItemFactura {
+
+    private int cantidad;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
